a78f74a317453c42206fcf7f5d0e2e3018bb92f4
Andrea Brandi <git@andreabrandi.com>
2021-01-20 23:19:19 +0100

    Set permission for resizerootfs.service Use 0644, default used by many system files.

--- scripts/bootstrap_resizerootfs.sh
+++ scripts/bootstrap_resizerootfs.sh
@@ -5,4 +5,5 @@ set -o errtrace -o nounset -o pipefail -o errexit
 # set up one shot resize root fs for first boot
 mv /tmp/resizerootfs/resizerootfs.service /etc/systemd/system
 mv /tmp/resizerootfs/resizerootfs /usr/sbin/
+chmod 0644 /etc/systemd/system/resizerootfs.service
 systemctl enable resizerootfs.service
